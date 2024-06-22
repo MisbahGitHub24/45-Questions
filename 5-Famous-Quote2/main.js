57a705a66af701c873a4c4798b7978cc486aa999
Misbah Sarwar <160399812+MisbahGitHub24@users.noreply.github.com>
2024-06-23 00:27:10 +0500

    Add files via upload

---
+++ 5-Famous-Quote2/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let famous_person = "Confucius"; //Confucius was a Chinese philosopher
+let qoute = `“Our greatest glory is not in never falling, but in rising every time we fall.”`;
+let message = `${famous_person} once said ${qoute}`;
+console.log(message);
---
+++ 5-Famous-Quote2/main.ts
@@ -0,0 +1,7 @@
+let famous_person:string ="Confucius";//Confucius was a Chinese philosopher
+
+let qoute:string =`“Our greatest glory is not in never falling, but in rising every time we fall.”`;
+
+let message:string =`${famous_person} once said ${qoute}`;
+
+console.log(message);
