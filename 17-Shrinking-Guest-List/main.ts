66010f85d6efc2934d6f8cdb642ef5541063bbf6
Misbah Sarwar <160399812+MisbahGitHub24@users.noreply.github.com>
2024-06-23 00:32:30 +0500

    Add files via upload

---
+++ 17-Shrinking-Guest-List/main.ts
@@ -0,0 +1,28 @@
+let guestsList:string []=["Fatima","Zainab","Areeba","Iqra","Mehar"]
+
+guestsList.splice(2,1,"Ambreen");
+
+guestsList.push("Laiba");
+
+guestsList.unshift("Rabia");
+
+guestsList.splice(2,0,"Ali")
+
+console.log("Sorry, I can only invite two people for dinner.");
+
+while (guestsList.length > 2) {
+    let removedGuest = guestsList.pop()
+    if (removedGuest) {
+        console.log(`Sorry ${removedGuest}, I can't invite you to dinner.`);
+    }
+}
+
+guestsList.forEach(guest => {
+    console.log(`Dear ${guest}, you're still invited to dinner.`);
+});
+
+guestsList.pop();
+guestsList.pop();
+
+
+console.log("Final guest list:", guestsList);
