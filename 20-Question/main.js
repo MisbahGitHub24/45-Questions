55ca1d89b3d11eee91001703068d41bd77568f3c
Misbah Sarwar <160399812+MisbahGitHub24@users.noreply.github.com>
2024-06-23 00:33:03 +0500

    Add files via upload

---
+++ 20-Question/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let countriesName = ["Australia", "Canada", "German", "pakistan", "Japan", "Iran", "Saudi Arab", "Pakistan", "India", "Iraq"];
+countriesName.forEach((country) => {
+    console.log(country);
+});
---
+++ 20-Question/main.ts
@@ -0,0 +1,8 @@
+let countriesName:string[] = ["Australia","Canada","German","pakistan","Japan","Iran","Saudi Arab","Pakistan","India","Iraq"]
+
+
+
+countriesName.forEach((country)=>{
+    console.log(country);
+    
+})
